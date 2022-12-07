94db2a504eacf948c0a7e65fb71f6e2c2e78dd48
David Zambrano <david._101@hotmail.com>
2022-12-07 15:44:01 -0500

    Add unit test for calculate and operate files

---
+++ src/test/operate.test.js
@@ -0,0 +1,24 @@
+import operate from '../components/logic/operate.js'
+
+describe('testing operation file', ()=> {
+ test('adtion', ()=>{
+  const result = operate(1,2,'+');
+  expect(result).toBe('3');
+ });
+ test('substraction', ()=>{
+  const result = operate(1,2,'-');
+  expect(result).toBe('-1');
+ });
+ test('multiplication', ()=>{
+  const result = operate(1,2,'x');
+  expect(result).toBe('2');
+ });
+ test('division', ()=>{
+  const result = operate(2,2,'÷');
+  expect(result).toBe('1');
+ });
+ test('modulus', ()=>{
+  const result = operate(3,2,'%');
+  expect(result).toBe('1');
+ })
+})
